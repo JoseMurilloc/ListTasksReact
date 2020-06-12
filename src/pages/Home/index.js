cbc2acfbbfb533ea94445c04562dd331e12cc2a9
Jose Murillo <jooseemurillo@gmail.com>
2020-06-12 17:45:17 -0300

    Arrays data fake to list Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './styles.css';
+import arrayTasks from '../../utils/tasks';
+
 import { AiOutlineEdit, AiFillDelete } from 'react-icons/ai';
 
 function Home() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(arrayTasks);
+  }, []);
+
   return (
     <>
     <header>
@@ -17,53 +25,22 @@ function Home() {
 
     <section>
       <ul>
-      <li>
-          <div className="container-list">
-            <div className="title-icons">
-              <h1>Adicionar um botão de voltar no site</h1>
-              <AiFillDelete size={25} />
-            </div>
+        {tasks.map(task => (
+          <li key={tasks.id}>
+            <div className="container-list">
+              <div className="title-icons">
+                <h1>{task.title}</h1>
+                <AiFillDelete size={25} />
+              </div>
             
-            <span>No site principal da facape tem um botão que esta quebrando o layout aos 650px de largura, resolve o problema quando tiver tempo.</span>
-            
-            <div className="icons-edit"> 
-              <AiOutlineEdit size={25} />
-            </div>
-
+              <span>{task.description}</span>
+              
+              <div className="icons-edit"> 
+                <AiOutlineEdit size={25} />
+              </div>
           </div>
         </li>
-
-        <li>
-          <div className="container-list">
-            <div className="title-icons">
-              <h1>Adicionar um botão de voltar no site</h1>
-              <AiFillDelete size={25} />
-            </div>
-            
-            <span>No site principal da facape tem um botão que esta quebrando o layout aos 650px de largura, resolve o problema quando tiver tempo.</span>
-            
-            <div className="icons-edit"> 
-              <AiOutlineEdit size={25} />
-            </div>
-
-          </div>
-        </li>
-
-        <li>
-          <div className="container-list">
-            <div className="title-icons">
-              <h1>Adicionar um botão de voltar no site</h1>
-              <AiFillDelete size={25} />
-            </div>
-            
-            <span>No site principal da facape tem um botão que esta quebrando o layout aos 650px de largura, resolve o problema quando tiver tempo.</span>
-            
-            <div className="icons-edit"> 
-              <AiOutlineEdit size={25} />
-            </div>
-
-          </div>
-        </li>
+        ))}
       </ul>
     </section>
     </>
